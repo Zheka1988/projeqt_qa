3b9571b6be8bbc5672b094483aa05d9c27a02b00
Zheka1988 <kveladze.kz@mail.ru>
2019-03-10 23:17:21 -0700

    added tests for sing in logout and registered user

--- spec/features/sign_reg_spec.rb
+++ spec/features/sign_reg_spec.rb
@@ -1,17 +1,40 @@
 require 'rails_helper'
 
-feature 'User can', %q{
+feature 'User can sign in', %q{
  Sign in,
  Log out,
  Register in the system
 } do
+  given(:user) { create(:user) }
 
-  scenario 'sign in system' do
-
+  scenario 'registered user tries sign in system' do
+    sign_in(user)
+    expect(page).to have_content 'Signed in successfully.'
   end
 
-  scenario 'sign out system'
-  scenario 'register in system'
+  scenario 'unregistered user tries sign in system' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+    expect(page).to have_content 'Invalid Email or password.'
+  end
 
+  scenario 'logged in user can logout system'  do
+    sign_in(user)
+    visit questions_path
+    click_on 'Logout'
+    expect(page).to have_content 'Signed out successfully.'
+  end
+
+  scenario 'register in system' do
+    visit new_user_registration_path
+
+    fill_in 'Email', with: 'new_user@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
 
 end
