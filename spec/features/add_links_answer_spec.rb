01eadaa90082a8171fd7df29e4254805a90c4718
Zheka1988 <kveladze.kz@mail.ru>
2019-05-27 23:10:45 -0700

    second commit

--- spec/features/add_links_answer_spec.rb
+++ spec/features/add_links_answer_spec.rb
@@ -8,22 +8,25 @@ feature 'User can add links to answer', %q{
 
   given(:user) { create(:user) }
   given(:question) { create :question, author: user }
-  given(:gist_url) { "https://gist.github.com/Zheka1988/1fface642803fb09e9d82ec4ccea17f5.js" }
-
+  # given(:gist_url) { "https://gist.github.com/Zheka1988/1fface642803fb09e9d82ec4ccea17f5.js" }
+  given(:google) { "https://google.ru" }
   scenario 'user can add linsk to answer', js: true do
     sign_in(user)
     visit question_path(question)
 
     fill_in 'Body', with: 'answer answer answer'
 
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
+    fill_in 'Link name', with: 'My link'
+    fill_in 'Url', with: google
 
     click_on 'Reply'
-
     within '.answers' do
-      expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'My link', href: google
     end
+
+    # within '.answers' do
+    #   expect(page).to have_link 'My gist', href: gist_url
+    # end
   end
 
 end
--- spec/features/add_links_question_spec.rb
+++ spec/features/add_links_question_spec.rb
@@ -7,8 +7,8 @@ feature 'User can add links to question', %q{
 } do
 
   given(:user) { create(:user) }
-  given(:gist_url) { "https://gist.github.com/Zheka1988/1fface642803fb09e9d82ec4ccea17f5.js" }
-
+  # given(:gist_url) { "https://gist.github.com/Zheka1988/1fface642803fb09e9d82ec4ccea17f5.js" }
+  given(:google) { "https://google.ru" }
   scenario 'user can add linsk to question' do
     sign_in(user)
     visit questions_path
@@ -17,13 +17,21 @@ feature 'User can add links to question', %q{
     fill_in 'Title', with: 'Text question'
     fill_in 'Body', with: 'text text text'
 
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
+    fill_in 'Link name', with: 'My link'
+    fill_in 'Url', with: google
 
 
     click_on 'Ask'
+    # save_and_open_page
+    expect(page).to have_link 'My link', href: google
 
-    expect(page).to have_link 'My gist', href: gist_url
+    # expect(page).to have_link 'My gist', href: gist_url
+    # within '.links' do #link-'+ Link.first.id.to_s do
+    #   expect(page).to have_selector (:css 'script', visible: false, count: 1) #"<script src='https://gist.github.com/Zheka1988/1fface642803fb09e9d82ec4ccea17f5.js'></script>")
+    # end
+    # all('script').find { |s| s[:src].nil? }.count.should_not be(0)
   end
+
+# "<script src='https://gist.github.com/Zheka1988/1fface642803fb09e9d82ec4ccea17f5.js'></script>"
 
 end
